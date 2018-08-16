ad27fc9d1d37a6e31cb36fb29ebb28eb21ef9608
Giorgio Garasto <mail@dabolus.app>
2018-08-16 17:38:27 +0200

    chore: ancilla -> ancillas

--- src/components/shell/template.js
+++ src/components/shell/template.js
@@ -31,7 +31,7 @@ export default function template({ appTitle, page, _drawerOpened, _offline, _sub
           <div class="name">Home</div>
         </a>
         <hr>
-        <a selected?="${page === 'ancilla'}" href="/ancilla">
+        <a selected?="${page === 'ancillas'}" href="/ancillas">
           <div class="icon">${ancillaIcon}</div>
           <div class="name">Ancilla Domini</div>
         </a>
@@ -53,6 +53,7 @@ export default function template({ appTitle, page, _drawerOpened, _offline, _sub
     <!-- Main content -->
     <main role="main" class="main-content">
       <home-page class="page" active?="${page === 'home'}"></home-page>
+      <ancillas-page class="page" active?="${page === 'ancillas'}"></ancillas-page>
       <songs-page class="page" active?="${page === 'songs'}" subroute="${_subroute}"></songs-page>
       <my-view3 class="page" active?="${page === 'view3'}"></my-view3>
       <my-view404 class="page" active?="${page === 'view404'}"></my-view404>
