b824d14bb44e74961395199db594b90f8fd55e0b
Giorgio Garasto <giorgio@garasto.it>
2020-04-20 07:34:31 +0200

    feat: use the new loading spinner in the load directive

--- pwa/src/helpers/directives.ts
+++ pwa/src/helpers/directives.ts
@@ -3,11 +3,17 @@ import { nothing } from 'lit-html';
 import { until } from 'lit-html/directives/until';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
+import '../components/loading-spinner/loading-spinner.component';
+
 export const load = <T>(
   promise: Promise<T>,
   onSuccess: (value: T) => TemplateResult,
   onFailure: (error: Error) => TemplateResult = () => html`${nothing}`,
-) => until(promise.then(onSuccess).catch(onFailure), html`<p>Loading...</p>`);
+) =>
+  until(
+    promise.then(onSuccess).catch(onFailure),
+    html`<loading-spinner></loading-spinner>`,
+  );
 
 const getParagraphClass = (paragraph: string) => {
   if (/^rit\./i.test(paragraph)) {
