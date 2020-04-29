a7dc6cc6bfb63d95d7dd5bf1d90c3c87114d67f2
Giorgio Garasto <giorgio@garasto.it>
2020-04-29 07:19:37 +0200

    chore: don't lazy load ancilla page parts

--- pwa/src/components/pages/ancillas/ancillas.template.ts
+++ pwa/src/components/pages/ancillas/ancillas.template.ts
@@ -1,19 +1,14 @@
 import { html } from 'lit-element';
-import { until } from 'lit-html/directives/until';
+import { cache } from 'lit-html/directives/cache';
 import { AncillasPage } from './ancillas.component';
 
-import '../../loading-spinner/loading-spinner.component';
+import '../../ancilla-viewer/ancilla-viewer.component';
+import '../../ancillas-list/ancillas-list.component';
 
 export default function template(this: AncillasPage) {
-  const subroute = this.subroute
-    ? import('../../pdf-viewer/pdf-viewer.component').then(
-        () => html`<pdf-viewer
-          src="https://firebasestorage.googleapis.com/v0/b/ancillas/o/processed%2FAD2_2018.pdf?alt=media"
-        ></pdf-viewer>`,
-      )
-    : import('../../ancillas-list/ancillas-list.component').then(
-        () => html`<ancillas-list></ancillas-list>`,
-      );
-
-  return until(subroute, html`<loading-spinner></loading-spinner>`);
+  return html`${cache(
+    this.subroute
+      ? html`<ancilla-viewer ancilla="${this.subroute}"></ancilla-viewer>`
+      : html`<ancillas-list></ancillas-list>`,
+  )}`;
 }
