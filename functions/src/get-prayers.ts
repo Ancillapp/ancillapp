763259d89e39884ac98d1ad9671b8784ea3531bd
Giorgio Garasto <giorgio@garasto.it>
2020-05-02 07:32:27 +0200

    feat: add support for full prayers data

--- functions/src/get-prayers.ts
+++ functions/src/get-prayers.ts
@@ -1,26 +1,31 @@
 import * as functions from 'firebase-functions';
 import { mongoDb } from './helpers/mongo';
 
-export const getPrayers = functions.https.onRequest(async (_, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
+export const getPrayers = functions.https.onRequest(
+  async ({ query: { fullData } }, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
 
-  const db = await mongoDb;
-  const prayersCollection = db.collection('prayers');
+    const db = await mongoDb;
+    const prayersCollection = db.collection('prayers');
 
-  const prayers = await prayersCollection
-    .find(
-      {},
-      {
-        projection: {
-          _id: 0,
-          slug: 1,
-          title: 1,
-          image: 1,
+    const prayers = await prayersCollection
+      .find(
+        {},
+        {
+          projection: {
+            _id: 0,
+            slug: 1,
+            title: 1,
+            image: 1,
+            ...(typeof fullData !== 'undefined' && {
+              content: 1,
+            }),
+          },
         },
-      },
-    )
-    .sort({ _id: 1 })
-    .toArray();
+      )
+      .sort({ _id: 1 })
+      .toArray();
 
-  res.json(prayers);
-});
+    res.json(prayers);
+  },
+);
