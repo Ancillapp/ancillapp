43abbb0aeb7580dbe9db5e187ffedf5a9f62f6ae
Giorgio Garasto <giorgio@garasto.it>
2020-05-02 07:30:11 +0200

    feat: add support for full songs data

--- functions/src/get-songs.ts
+++ functions/src/get-songs.ts
@@ -1,25 +1,40 @@
 import * as functions from 'firebase-functions';
 import { mongoDb } from './helpers/mongo';
 
-export const getSongs = functions.https.onRequest(async (_, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
+export const getSongs = functions.https.onRequest(
+  async ({ query: { fullData } }, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
 
-  const db = await mongoDb;
-  const songsCollection = db.collection('songs');
+    const db = await mongoDb;
+    const songsCollection = db.collection('songs');
 
-  const songs = await songsCollection
-    .find(
-      {},
-      {
-        projection: {
-          _id: 0,
-          number: 1,
-          title: 1,
+    const songs = await songsCollection
+      .find(
+        {},
+        {
+          projection: {
+            _id: 0,
+            number: 1,
+            title: 1,
+            ...(typeof fullData !== 'undefined' && {
+              content: 1,
+            }),
+          },
         },
-      },
-    )
-    .sort({ _id: 1 })
-    .toArray();
+      )
+      .toArray();
 
-  res.json(songs);
-});
+    res.json(
+      songs.sort(({ number: a }, { number: b }) => {
+        const normalizedA = a.replace('bis', '').padStart(4, 0);
+        const normalizedB = b.replace('bis', '').padStart(4, 0);
+
+        if (normalizedA === normalizedB) {
+          return b.endsWith('bis') ? -1 : 1;
+        }
+
+        return normalizedA < normalizedB ? -1 : 1;
+      }),
+    );
+  },
+);
