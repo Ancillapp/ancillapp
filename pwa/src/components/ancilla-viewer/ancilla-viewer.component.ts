d4555a4dc36a2125011c7ceb004f261b58bdaf2b
Giorgio Garasto <giorgio@garasto.it>
2020-06-02 09:36:29 +0200

    feat: correctly update title and description when viewing an ancilla

--- pwa/src/components/ancilla-viewer/ancilla-viewer.component.ts
+++ pwa/src/components/ancilla-viewer/ancilla-viewer.component.ts
@@ -1,4 +1,5 @@
 import { customElement, property } from 'lit-element';
+import { updateMetadata } from 'pwa-helpers';
 import { localize } from '../../helpers/localize';
 import { localizedPages } from '../../helpers/localization';
 import { withTopAppBar } from '../../helpers/with-top-app-bar';
@@ -9,6 +10,10 @@ import styles from './ancilla-viewer.styles';
 import template from './ancilla-viewer.template';
 
 import { apiUrl } from '../../config/default.json';
+
+import firebase from 'firebase/app';
+
+const analytics = firebase.analytics();
 
 export interface Ancilla {
   code: string;
@@ -50,7 +55,24 @@ export class AncillaViewer extends localize(withTopAppBar(PageViewElement)) {
         `${apiUrl}/ancillas/${ancillaCode}`,
       ).then((res) => res.json());
 
-      const { code } = await this._ancillaPromise;
+      const {
+        code,
+        name: { [this.locale]: localizedName },
+      } = await this._ancillaPromise;
+
+      const pageTitle = `Ancillapp - ${this.localeData.ancillas} - ${localizedName}`;
+
+      updateMetadata({
+        title: pageTitle,
+        description: this.localeData.ancillaDescription(localizedName),
+      });
+
+      analytics.logEvent('page_view', {
+        page_title: pageTitle,
+        page_location: window.location.href,
+        page_path: window.location.pathname,
+        offline: false,
+      });
 
       if (ancillaCode !== code) {
         window.history.replaceState(
