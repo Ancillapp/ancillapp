cded575ae29ff163947ec375be64c80c530f06b7
Giorgio Garasto <giorgio@garasto.it>
2020-06-07 20:51:34 +0200

    chore: directly use songs container to get first song reference

--- pwa/src/containers/songs-list/songs-list.component.ts
+++ pwa/src/containers/songs-list/songs-list.component.ts
@@ -296,8 +296,8 @@ export class SongsList extends localize(withTopAppBar(PageViewElement)) {
         this._refreshSongs();
       }
 
-      const firstSong = this.shadowRoot!.querySelector<HTMLAnchorElement>(
-        '.songs-container > .songs-batch-container > a',
+      const firstSong = this._songsContainer!.querySelector<HTMLAnchorElement>(
+        '.songs-batch-container > a',
       );
 
       if (!firstSong) {
