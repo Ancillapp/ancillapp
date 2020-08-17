abf4af4226e8593d93bca45cdbeda1dba2d5b1a1
Giorgio Garasto <giorgio@garasto.it>
2020-08-17 16:12:20 +0200

    fix: wrong input click area

--- pwa/src/components/search-top-bar/search-top-bar.template.ts
+++ pwa/src/components/search-top-bar/search-top-bar.template.ts
@@ -4,13 +4,14 @@ import { SearchTopBar } from './search-top-bar.component';
 export default function template(this: SearchTopBar) {
   return html`
     <header style="transform: translateY(${this._scrollFromTop}px)">
-      <div class="row" @click="${this._handleSearchClick}">
+      <div class="row">
         <section class="start">
           <slot name="leadingIcon"></slot>
         </section>
         <input
           placeholder="${this.placeholder}"
           @input="${this._handleSearch}"
+          @click="${this._handleSearchClick}"
         />
         <section class="end">
           <slot name="trailingIcon"></slot>
