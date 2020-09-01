dd750ffb3582cc7606809a779fa9fee791a194b2
Giorgio Garasto <giorgio@garasto.it>
2020-09-01 08:51:42 +0200

    refactor: make firebase lazy load when needed

--- pwa/src/helpers/firebase.ts
+++ pwa/src/helpers/firebase.ts
@@ -1,16 +1,111 @@
+import type Firebase from 'firebase';
+
 import config from '../config/default.json';
 
+let firebase: typeof Firebase;
+let authInitialized = false;
+let analyticsInitialized = false;
+
 const initializeFirebase = async () => {
-  const { default: firebase } = await import('firebase/app');
+  if (!firebase) {
+    const { default: importedFirebase } = await import('firebase/app');
 
-  await Promise.all([import('firebase/auth'), import('firebase/analytics')]);
+    importedFirebase.initializeApp(config.firebase);
 
-  firebase.initializeApp(config.firebase);
+    firebase = importedFirebase;
+  }
 
   return firebase;
 };
 
-export const firebasePromise = initializeFirebase();
+const initializeAuth = async () => {
+  await initializeFirebase();
+
+  if (!authInitialized) {
+    await import('firebase/auth');
+    authInitialized = true;
+  }
+};
+
+export enum Provider {
+  GOOGLE = 'google',
+  FACEBOOK = 'facebook',
+  TWITTER = 'twitter',
+  MICROSOFT = 'microsoft',
+  APPLE = 'apple',
+  GITHUB = 'github',
+}
+
+const providersMap: Record<
+  Provider,
+  (auth: typeof Firebase.auth, locale: string) => firebase.auth.AuthProvider
+> = {
+  google: (auth) => new auth.GoogleAuthProvider(),
+  facebook: (auth) => new auth.FacebookAuthProvider(),
+  twitter: (auth) => new auth.TwitterAuthProvider(),
+  microsoft: (auth) => new auth.OAuthProvider('microsoft.com'),
+  apple: (auth, locale) => {
+    const provider = new auth.OAuthProvider('apple.com');
+
+    provider.setCustomParameters({ locale });
+
+    return provider;
+  },
+  github: (auth) => new auth.GithubAuthProvider(),
+};
+
+export const loginWithProvider = async (provider: Provider, locale: string) => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+
+  const configuredProvider = providersMap[provider](firebase.auth, locale);
+
+  await firebase.auth().signInWithPopup(configuredProvider);
+};
+
+export const loginWithEmailAndPassword = async (
+  email: string,
+  password: string,
+) => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+
+  await firebase.auth().signInWithEmailAndPassword(email, password);
+};
+
+export const signupWithEmailAndPassword = async (
+  email: string,
+  password: string,
+) => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+
+  await firebase.auth().createUserWithEmailAndPassword(email, password);
+};
+
+export const requestPasswordReset = async (email: string, url: string) => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+
+  await firebase.auth().sendPasswordResetEmail(email, { url });
+};
+
+export const logout = async () => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+
+  await firebase.auth().signOut();
+};
+
+export const onAuthStateChanged = async (
+  cb: (user: Firebase.User | null) => void,
+) => {
+  const firebase = await initializeFirebase();
+  await initializeAuth();
+  firebase.auth.GoogleAuthProvider;
+
+  firebase.auth().onAuthStateChanged(cb);
+};
 
 export const logEvent = async (
   eventName: Parameters<
@@ -23,7 +118,12 @@ export const logEvent = async (
     import('firebase/app').analytics.Analytics['logEvent']
   >[2],
 ) => {
-  const firebase = await firebasePromise;
+  const firebase = await initializeFirebase();
+
+  if (!analyticsInitialized) {
+    await import('firebase/analytics');
+    analyticsInitialized = true;
+  }
 
   firebase.analytics().logEvent(
     eventName,
