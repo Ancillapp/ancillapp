a1b8dc198a53ac9d73528404281ce5244a43c59f
Giorgio Garasto <giorgio@garasto.me>
2022-10-02 15:49:16 -0300

    fix: replace all brs with newlines

--- functions/src/api/handlers/holy-mass/liturgy-scrapers/helpers.ts
+++ functions/src/api/handlers/holy-mass/liturgy-scrapers/helpers.ts
@@ -1,6 +1,6 @@
 import sanitizeHtml from 'sanitize-html';
 
 export const dropHtml = (str: string) =>
-  sanitizeHtml(str.replace(/(?:\s*<br\s*\/?>\s*)+/, '\n'), {
+  sanitizeHtml(str.replace(/(?:\s*<br\s*\/?>\s*)+/gi, '\n'), {
     allowedTags: [],
   }).trim();
