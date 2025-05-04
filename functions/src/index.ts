ef7c7f8b5c98de11a4b8a7efed068cd5a3412611
Giorgio Garasto <giorgio@garasto.me>
2025-05-04 09:43:09 +0200

    chore(functions): move to milan

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
+import { setGlobalOptions } from 'firebase-functions/options';
 import { api } from './api';
 import { processAncilla } from './process-ancilla';
+
+setGlobalOptions({ region: 'europe-west8' });
 
 export const pwa = { api, processAncilla };
